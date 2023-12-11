5298df36b7a8e53e710c20ca91ecdce28f5e23d8
okt <web@evanfiddes.com>
2023-12-11 00:29:23 -0700

    Update config.h: Set TAPPING_TOGGLE to 2 and change RGB_MATRIX_DEFAULT_MODE to RGB_MATRIX_NONE

--- keyboards/nuphy/air75_v2/ansi/config.h
+++ keyboards/nuphy/air75_v2/ansi/config.h
@@ -20,11 +20,12 @@
 #define SD1_RX_PIN                          B7
 #define SD1_RX_PAL_MODE                     0
 
+#define TAPPING_TOGGLE                      2
 #define TAP_CODE_DELAY                      8
 #define DYNAMIC_KEYMAP_MACRO_DELAY          8
 #define DYNAMIC_KEYMAP_LAYER_COUNT          8
 
 #define EECONFIG_USER_DATA_SIZE             8
 
-#define RGB_MATRIX_DEFAULT_MODE             RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define RGB_MATRIX_DEFAULT_MODE             RGB_MATRIX_NONE
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
